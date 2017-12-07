265939b58e90a9f8661318e3ef7875eb26d15d28
Kevin <736074781@qq.com>
2017-12-07 06:29:02 +0000

    User can create topic

--- resources/views/layouts/_message.blade.php
+++ resources/views/layouts/_message.blade.php
@@ -1,6 +1,6 @@
 @if (Session::has('message'))
     <div class="alert alert-info">
-        <button type="button" name="button" class="colse" data-dismiss="alert" aria-hidden="true">×</button>
+        <button type="button" name="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
         {{ Session::get('message') }}
     </div>
 @endif
