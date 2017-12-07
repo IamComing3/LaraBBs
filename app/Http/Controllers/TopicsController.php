cbda1dbe2233475583515352b88ae2782dbecc83
Kevin <736074781@qq.com>
2017-12-07 09:13:17 +0000

    Author can delete topic

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -42,7 +42,7 @@ class TopicsController extends Controller
 		$topic->fill($request->all());
         $topic->user_id = Auth::id();
         $topic->save();
-		return redirect()->route('topics.show', $topic->id)->with('message', '成功创建话题~');
+		return redirect()->route('topics.show', $topic->id)->with('success', '成功创建话题~');
 	}
 
 	public function edit(Topic $topic)
@@ -57,7 +57,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', '更新成功~');
+		return redirect()->route('topics.show', $topic->id)->with('success', '更新成功~');
 	}
 
 	public function destroy(Topic $topic)
@@ -65,7 +65,7 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', '删除成功~');
+		return redirect()->route('topics.index')->with('success', '删除成功~');
 	}
 
     public function uploadImage(Request $request, ImageUploadHandler $uploader)
